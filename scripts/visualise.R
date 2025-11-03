7a2705964f24252759f63128620f2cdf1b17251b
camappel <camdappel@gmail.com>
2025-11-03 18:34:58 +0000

    simplify

--- scripts/visualise.R
+++
@@ -1,8 +0,0 @@
-# Import Porstmouth data
-porstmouth <- read.csv("data/Portsmouth.csv")
-
-print the first 7 days of the data
-print(porstmouth[1:7,])
-
-# Plot the first 24 hours of the data
-plot(porstmouth$time[1:24], porstmouth$elevation[1:24], type = "l")
